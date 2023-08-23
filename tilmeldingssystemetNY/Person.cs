1b78bddce1676f804b3984d43077bcd847cd37d2
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-08-24 00:16:45 +0200

    Add project files.

---
+++ tilmeldingssystemetNY/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tilmeldingssystemetNY
+{
+    internal class Person
+    {
+
+        // instansfelter
+        private int _id;
+        private string _navn;
+        private string _adresse;
+        private string _patrulje;
+        private DateTime _fødselsdato;
+        private bool _enUge;
+
+        // properties
+
+        public int ID { get; }
+        public string Navn { get; }
+
+        public string Adresse { get; }
+
+        public string Patrulje { get; }
+
+        public DateTime FødselsDato { get; }
+
+        public bool EnUge { get; }
+
+
+
+        // konstruktør
+
+        public Person(int id, string navn, string adresse, string patrulje, DateTime fødselsDato, bool enUge)
+        {
+            ID = id;
+            Navn = navn;
+            Adresse = adresse;
+            Patrulje = patrulje;
+            FødselsDato = fødselsDato;
+            enUge = EnUge;
+
+        }
+
+        // ToString() metode
+
+        public override string ToString()
+        {
+            return $" ID: {ID}, Navn: {Navn}, Adresse: {Adresse}, Patrulje: {Patrulje}, Fødselsdato: {FødselsDato}, Tilmeldt en uge: {EnUge}";
+        }
+
+    }
+}
---
+++ tilmeldingssystemetNY/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace tilmeldingssystemetNY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanser af person klassen:
+
+            Person person1 = new Person(1, "Louise", "Roskilde", "Roskilde patrulje", DateTime.Parse("19/02/1999"), true);
+
+            Console.WriteLine(person1);
+
+            Person person2 = new Person(2, "Mahmoud", "Vanløse", "Vanløse patrulje", DateTime.Parse("10/02/1998"), false);
+
+            Console.WriteLine(person2);
+
+        }
+    }
+}
